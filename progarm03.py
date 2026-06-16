2c36711325b28d458d87b04e75cfd1b83802023d
164263-pixel <164263@wsk.ac.th>
2026-06-16 03:01:57 +0000

    program03.py

---
+++ progarm03.py
@@ -0,0 +1,7 @@
+width = float(input("กรุณากรอบความกว้าง:")) 
+length = float(input("กรุณากรอบความยาว:"))
+
+area = width * length #คำนวณพื้นที่
+
+print("พื้นที่รูปสีเหลี่ยมมุมฉาก =",area) # แสดงผลลัพธ์
+print("author: Phongsaran Homeaing M.6/9")
